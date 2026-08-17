12e109599c620a51f3c063c2f6afb88998fa7020
SHKim9020 <53808062+SHKim9020@users.noreply.github.com>
2026-08-18 01:52:51 +0900

    Connect Arduino Studio to Supabase

--- arduino-studio/cloud-config.js
+++ arduino-studio/cloud-config.js
@@ -1,4 +1,4 @@
 window.ONEMAKER_CLOUD_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://kqeoriazxmzjltubheod.supabase.co",
+  supabasePublishableKey: "sb_publishable_ZHjN9Pi_h6R5bIIryK7R0g_aJ-VeBgh"
 };
